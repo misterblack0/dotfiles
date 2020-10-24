01fac51608418ad44cc0f2f79555d414a82f218b
misterblack0 <misterblack0@protonmail.com>
2020-10-24 21:56:52 +0300

    remake

--- post_install.sh
+++ post_install.sh
@@ -1,104 +1,98 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root"
-    exit 1
-else
-    #Update and Upgrade
-    echo "Updating and Upgrading"
-    apt-get update && sudo apt-get upgrade -y
+# Installation of many packages in a fresh Mint
 
-    sudo apt-get install dialog
-    cmd=(dialog --separate-output --checklist "Select the name of the software you want to install :" 22 76 16)
-    options=(1 "Build Dependencies" on # any option can be set to default to "on"
-        2 "git" on
-        3 "Node.js & Npm" on
-        4 "Brave Browser" on
-        5 "Visual Studio Code" on
-        6 "Filezilla" on
-        7 "htop" on
-        8 "iftop" on
-        9 "ncdu" on
-        10 "Virtual Box" on
-        11 "Gimp" on
-        12. "Shutter" on)
-    choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-    clear
-    for choice in $choices; do
-        case $choice in
-        1)
-            #Install dependencies*
-            echo "Installing dependencies"
-            apt install build-essential curl wget software-properties-common apt-transport-https -y
-            ;;
+###################################
+# Add repositories
+###################################
+echo
+echo "Installing repositories..."
+echo
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo add-apt-repository ppa:otto-kesselgulasch/gimp
 
-        2)
-            #Install git
-            echo "Installing git"
-            apt install git -y
-            ;;
-        3)
-            #Install Node.js
-            echo "Installing nodejs & npm"
-            apt install npm -y
-            ;;
-        4)
-            #Install Brave Browser
-            echo "Software-properties-common"
-            curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-            echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-            apt-get update
-            apt install brave-browser -y
-            ;;
-        5)
-            #Install Visual Studio Code
-            echo "Installing Visual Studio Code"
-            add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-            apt-get update
-            apt install code -y
-            ;;
-        6)
-            #Install Filezilla
-            echo "Installing Filezilla"
+###################################
+# Update and Upgrade
+###################################
+echo
+echo "Updating and upgrading..."
+echo
+sudo apt-get update && sudo apt-get upgrade -y
 
-            apt install filezilla -y
-            ;;
-        7)
-            #Install htop
-            echo "Installing htop"
+###################################
+# Remove programs not used
+###################################
+echo
+echo "Removing programs not used..."
+echo
+sudo apt-get remove hexchat hexchat-common thunderbird thunderbird-gnome-support thunderbird-locale-en thunderbird-locale-en-us banshee tomboy pidgin pidgin-libnotify -y
 
-            apt install htop -y
-            ;;
-        8)
-            #Install iftop
-            echo "Installing iftop"
+###################################
+# Installations
+###################################
+echo
+echo "Installing programs..."
+echo
 
-            apt install iftop -y
-            ;;
-        9)
-            #Install ncdu
-            echo "Installing ncdu"
+## system
+### important
+sudo apt-get install tasksel gparted p7zip-full p7zip-rar -y
+### desktop
+sudo apt-get remove -y
+sudo apt-get install xdotool gnome-screensaver -y
+### nautilus complements
+sudo apt-get install nautilus nautilus-dropbox nautilus-open-terminal -y
+### java and pdf tools
+sudo apt-get install oracle-java7-installer
+### pdf tools
+sudo apt-get install acroread cups-pdf -y
+## audio, image and video
+sudo apt-get install inkscape
+### tools
+sudo apt-get install mencoder vlc audacious skype brasero -y
+### torrent and direct download
+sudo apt-get install jdownloader -y
 
-            apt install ncdu -y
-            ;;
-        10)
-            #Install Virtual Box
-            echo "Installing Virtual Box"
+## developing
+### compilers and IDEs
+sudo apt-get install build-essential gcc-avr avr-libc doxygen doxygen-latex cmake cmake-curses-gui gfortran libgtk2.0-dev pkg-config -y
+sudo apt-get install qtcreator qtcreator-plugin-cmake -y
+### libraries
+sudo apt-get install libboost-all-dev libeigen3-dev libblas-dev liblapack-dev ant -y
+### python
+sudo apt-get install python-tk python-matplotlib python-pip -y
+sudo easy_install ipython jinja2 tornado pyzmq scipy scikit-image
+### repositories
+sudo apt-get install git ssh openssh-server filezilla filezilla-common -y
+### tools
+sudo apt-get install kdiff3-qt vim source-highlight -y
+### latex
+sudo apt-get install texlive-latex-base texlive-base texlive-latex-extra texlive-font-utils texlive-fonts-recommended texlive-generic-recommended texlive-generic-extra texlive-omega texlive-plain-extra texlive-extra-utils texlive-lang-spanish texlive-lang-english texlive-pictures texlive-math-extra texlive-science -y
+sudo apt-get install gedit-plugins/trusty gedit-latex-plugin -y
+### web tools
+echo "To install LAMP:"
+#echo "Launch Synaptic. Edit Menu, Package by task, LAMP Server"
+sudo apt-get install php5 -yy
+sudo apt-get install mysql-client mysql-server mysql-workbench -y
+sudo apt-get install phpmyadmin phpsysinfo -y
+###################################
+# Cleaning
+###################################
+echo
+echo "Cleaning..."
+sudo apt-get autoclean
 
-            apt install virtualbox -y
-            ;;
-        11)
-            #Install Gimp
-            echo "Installing Gimp"
-            add-apt-repository ppa:otto-kesselgulasch/gimp
-            apt update
-            apt install gimp -y
-            ;;
-        12)
-            #Install Shutter
-            echo "Installing Shutter"
-            apt install shutter gnome-web-photo libgoo-canvas-perl -y
-            ;;
-        esac
-    done
-fi
+###################################
+# Configuring
+###################################
+echo
+echo "Configuring..."
+## java
+sudo update-alternatives --config java
+
+###################################
+# Finish!
+###################################
+echo
+echo "Finish! All done!"
+echo
