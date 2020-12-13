00be72965d3289f73a79a991091777525961042f
misterblack0 <misterblack0@protonmail.com>
2020-12-13 17:37:27 +0200

    eslint

---
+++ install_commands.sh
@@ -0,0 +1,12 @@
+npm i -D eslint \
+         prettier \
+         eslint-plugin-react \
+         eslint-plugin-react-hooks \
+         eslint-config-prettier \
+         eslint-plugin-prettier \
+         eslint-plugin-jsx-a11y 
+# Import sort plugin
+npm i -D eslint-plugin-simple-import-sort
+
+# Husky and lint-staged
+npm i -D husky lint-staged
