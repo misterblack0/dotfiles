0ff0b004e4ce3303468a085468a229101f6a1cb6
misterblack0 <misterblack0@protonmail.com>
2020-12-26 12:07:13 +0200

    alias script

---
+++ add_alias.sh
@@ -0,0 +1,14 @@
+#My custom aliases
+#alias uu='sudo apt update && sudo apt upgrade -y'
+#alias ..='cd ..'
+#alias ...='cd ../../../'
+#alias ....='cd ../../../../'
+#alias .....='cd ../../../../'
+#alias .4='cd ../../../../'
+#alias .5='cd ../../../../..'
+#alias grep='grep --color=auto'
+#alias egrep='egrep --color=auto'
+#alias fgrep='fgrep --color=auto'
+
+
+echo "alias uu='sudo apt update && sudo apt upgrade -y'" >> /home/mrblack/.bashrc
